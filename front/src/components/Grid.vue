a344a33ab2ab0507437ef26b665492625874aa44
Sabino Velasquez <sabinovelasquez@gmail.com>
2018-02-21 20:49:38 -0300

    grid && header

--- front/src/components/Grid.vue
+++ front/src/components/Grid.vue
@@ -2,23 +2,34 @@
   el-main
     .grid
       el-row.masonry-container(:gutter='20', v-masonry='true', transition-duration='0.12s', item-selector='.item')
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:300px') 1
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:450px') 2
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:230px') 3
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:150px') 4
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:320px') 5
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:190px') 6
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:440px') 7
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:390px') 8
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:300px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:450px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:230px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:150px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:320px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:190px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:440px') 
+          .content
+        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:390px') 
+          .content
 </template>
 <style lang='scss'>
 @import '../styles/grid.scss';
 .grid{
   margin:10px;
 }
-.item {
-  border: 1px solid red;
-  background-color: gray;
+.item{
+  padding: 10px 0 10px;
+}
+.content {
+  height: 100%;
+  background-color: #F3F3F3;
 }
 </style>
 
--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,11 +1,24 @@
 <template lang='pug'>
 nav.nav
-  ul.left
-    li
-      router-link(to='/') Home
-  ul.right
-    li
-      router-link(to='/detail') Detalle
+  el-row
+    el-col(:span='8')
+      ul
+        li
+          router-link(to='/') Home
+        li
+          router-link(to='/detail') Detalle
+    el-col.align-center.logo(:span='8')
+      img(src='../assets/da-lettering.svg', alt='Dani Aguilera Lettering')
+    el-col(:span='8')
+  //- ul.left
+  //-   li
+  //-     router-link(to='/') Home
+  //- ul.center
+  //-   li
+  //-     img(src='../assets/da-lettering.svg', alt='Dani Aguilera Lettering')
+  //- ul.right
+  //-   li
+  //-     router-link(to='/detail') Detalle
 </template>
 
 <style lang='scss'>
