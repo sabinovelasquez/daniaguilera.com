131c361606b1e1f20b189f839bf71feac76fd3ba
Sabino Velasquez <sabinovelasquez@gmail.com>
2018-02-22 00:25:56 -0300

    directive render from instagram

--- front/src/components/Grid.vue
+++ front/src/components/Grid.vue
@@ -1,44 +1,41 @@
 <template lang='pug'>
-  el-main
-    .grid
-      el-row.masonry-container(:gutter='20', v-masonry='true', transition-duration='0.12s', item-selector='.item')
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:300px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:450px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:230px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:150px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:320px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:190px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:440px') 
-          .content
-        el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', style='height:390px') 
-          .content
+el-main
+  el-row.loading(v-if='!items')
+    i.el-icon-loading
+  .grid
+    el-row.masonry-container(:gutter='20', v-masonry='true', transition-duration='0.12s', item-selector='.item')
+      el-col.item(:xs='24', :sm='12', :md='6', :xl='3', v-masonry-tile='true', v-for='item in items', :key='item.id')
+        .content
+          img.responsive(v-bind:src='item.display_src')
 </template>
+
 <style lang='scss'>
 @import '../styles/grid.scss';
-.grid{
-  margin:10px;
-}
-.item{
-  padding: 10px 0 10px;
-}
-.content {
-  height: 100%;
-  background-color: #F3F3F3;
-}
 </style>
 
 <script>
 import Vue from 'vue'
 import { VueMasonryPlugin } from 'vue-masonry'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
 Vue.use(VueMasonryPlugin)
 export default {
-  name: 'Grid'
+  name: 'Grid',
+  data () {
+    return {
+      title: '',
+      items: null
+    }
+  },
+  mounted () {
+    this.$http.get('https://www.instagram.com/da_lettering/?__a=1').then(response => {
+      this.items = response.body.user.media.nodes
+      this.$redrawVueMasonry()
+    }, error => {
+      console.error(error)
+    })
+  }
 }
+
 </script>
